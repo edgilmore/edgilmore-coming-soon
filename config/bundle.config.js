a51578a297d4c78d109e832cf161f52162753838
Ed Gilmore <edgilmore@live.com>
2016-02-13 15:02:39 -0700

    added bundle config for vendor library bundling added vendor.scss for vendor css libraries. updated bower.json and removed unused libraries added new tasks to gulp add new node dependencies

---
+++ config/bundle.config.js
@@ -0,0 +1,11 @@
+/**
+ * Created by Edward on 2/12/2016.
+ */
+module.exports = {
+    bundle: {
+        vendor: {
+            scripts: ['./bower_components/jquery/dist/jquery.js','./bower_components/bootstrap-sass/javascripts/bootstrap.js','./bower_components/timecircles/inc/TimeCircles.js','',''],
+            styles: ['./css/font-awesome.css', './css/vendor.css']
+        }
+    }
+};
--- gulpfile.js
+++ gulpfile.js
@@ -5,15 +5,21 @@
 var gulp = require('gulp');
 var del = require('del');
 var sass = require('gulp-sass');
+var bundle = require('gulp-bundle-assets');
 
 var config = {
     paths: {
-        bootstrapSass:'./bower_components/bootstrap-sass/assets/stylesheets/_bootstrap.scss',
+        vendorSassFile:'./sass/vendor.scss',
         fontsFiles:['./bower_components/bootstrap-sass/assets/fonts/bootstrap/*.*','./bower_components/font-awesome/fonts/*.*'],
         fontsDir: './fonts/',
         fontAwesomeSass:'./bower_components/font-awesome/scss/font-awesome.scss',
         styles:'./css/',
-        javascript: './js/'
+        stylesVendorDir: './css/vendor/',
+        javascript: './js/',
+        jsVendorDir: './js/vendor/',
+        temp: './temp/',
+        vegasOverlays: './bower_components/vegas/dist/overlays/',
+        images: './images/'
     }
 };
 gulp.task('clean:css', function () {
@@ -21,9 +27,9 @@ gulp.task('clean:css', function () {
 });
 gulp.task('clean:fonts', function() {
     return del(config.paths.fontsDir);
-})
-gulp.task('build:bootstrap',['clean:css', 'clean:fonts'], function() {
-    return gulp.src(config.paths.bootstrapSass)
+});
+gulp.task('build:vendor-css', function() {
+     return gulp.src(config.paths.vendorSassFile)
         .pipe(sass({outputStyle: 'extended'}).on('error', sass.logError))
         .pipe(gulp.dest(config.paths.styles));
 });
@@ -36,7 +42,10 @@ gulp.task('move:fonts', function () {
     return gulp.src(config.paths.fontsFiles)
         .pipe(gulp.dest(config.paths.fontsDir));
 });
-
-gulp.task('default',['build:bootstrap', 'build:font-awesome', 'move:fonts'], function () {
+gulp.task('move:vegas-overlays', function () {
+    return gulp.src(config.paths.vegasOverlays)
+        .pipe(gulp.dest(config.paths.images));
+});
+gulp.task('default',['build:vendor-css', 'build:font-awesome', 'move:fonts', 'move:vegas-overlays'], function () {
 
 });
